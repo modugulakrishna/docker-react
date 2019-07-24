5db8df030302be88c3c2a43b18611997f55372ec
modugulakrishna <moduglakrishna99@gmail.com>
2019-07-24 12:49:19 +0530

    my commit

--- src/App.test.js
+++ src/App.test.js
@@ -8,4 +8,10 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
 
